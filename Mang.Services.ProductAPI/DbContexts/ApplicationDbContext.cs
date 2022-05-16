cba399f56167c323067d3dbeaf0423883cb00eb9
Younes Alturkey <hi@younes.ninja>
2022-05-16 13:40:50 +0300

    add: latest progress

---
+++ Mang.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mang.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mang.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
