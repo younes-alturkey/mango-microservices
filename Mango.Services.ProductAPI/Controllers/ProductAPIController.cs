7c77e2f1504b0a1f3fdb3d0d9e4af615d51c34e4
Younes Alturkey <hi@younes.ninja>
2022-05-24 22:41:19 +0300

    add: latest work

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -17,7 +17,6 @@ namespace Mango.Services.ProductAPI.Controllers
             this._response = new ResponseDto();
         }
 
-        [Authorize]
         [HttpGet]
         public async Task<object> Get()
         {
@@ -34,7 +33,6 @@ namespace Mango.Services.ProductAPI.Controllers
             return _response;
         }
 
-        [Authorize]
         [HttpGet]
         [Route("{id}")]
         public async Task<object> Get(int id)
--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,14 +1,40 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+        public HomeController(IProductService productService)
         {
-            return View();
+            _productService = productService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<ProductDto> list = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>("");
+            if(response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details(int productId)
+        {
+            ProductDto model = new();
+            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId, "");
+            if (response != null && response.IsSuccess)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto> (Convert.ToString(response.Result));
+            }
+            return View(model);
         }
 
         [Authorize]
